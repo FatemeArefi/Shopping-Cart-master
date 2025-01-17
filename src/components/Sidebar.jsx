3300c262f41ce98ef69aef6dc9820395ccb8f393
FatemeArefi <f.arefi982@gmail.com>
2025-01-18 03:16:34 +0330

    responsive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,54 @@
+// import { FaListUl } from "react-icons/fa";
+// import { createQueryObject } from "../helpers/helper";
+// import styles from "./Sidebar.module.css";
+// import { categories } from "../constants/list";
+
+// function Sidebar({ query, setQuery }) {
+//   const categoryHandler = (event) => {
+//     const { tagName } = event.target;
+//     const category = event.target.innerText.toLowerCase();
+
+//     if (tagName !== "LI") return;
+//     setQuery((query) => createQueryObject(query, { category }));
+//   };
+
+//   return (
+//     <div className={styles.sidebar}>
+//       <div>
+//         <FaListUl />
+//         <p>Categories</p>
+//       </div>
+//       <ul onClick={categoryHandler}>
+//         {categories.map((i) => (
+//           <li
+//             key={i.id}
+//             className={
+//               i.type.toLowerCase() === query.category ? styles.selected : null
+//             }
+//           >
+//             {i.type}{" "}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Sidebar;
+
+import React, { useState } from "react";
 import { FaListUl } from "react-icons/fa";
 import { createQueryObject } from "../helpers/helper";
 import styles from "./Sidebar.module.css";
 import { categories } from "../constants/list";
 
 function Sidebar({ query, setQuery }) {
+  const [showCategories, setShowCategories] = useState(false);
+
+  const toggleCategories = () => {
+    setShowCategories(!showCategories);
+  };
+
   const categoryHandler = (event) => {
     const { tagName } = event.target;
     const category = event.target.innerText.toLowerCase();
@@ -14,22 +59,24 @@ function Sidebar({ query, setQuery }) {
 
   return (
     <div className={styles.sidebar}>
-      <div>
+      <div className={styles.categoryToggle} onClick={toggleCategories}>
         <FaListUl />
         <p>Categories</p>
       </div>
-      <ul onClick={categoryHandler}>
-        {categories.map((i) => (
-          <li
-            key={i.id}
-            className={
-              i.type.toLowerCase() === query.category ? styles.selected : null
-            }
-          >
-            {i.type}{" "}
-          </li>
-        ))}
-      </ul>
+      {showCategories && (
+        <ul className={styles.categoryList} onClick={categoryHandler}>
+          {categories.map((i) => (
+            <li
+              key={i.id}
+              className={
+                i.type.toLowerCase() === query.category ? styles.selected : null
+              }
+            >
+              {i.type}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
